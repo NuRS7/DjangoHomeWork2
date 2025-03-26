cc3f75bd6efb04696b10973291f1271a5e14dbc5
Nurs <nursultanerbakytuly88@gmail.com>
2025-03-26 18:15:10 +0500

    Project4

--- djangoapp/new_app/news/urls.py
+++ djangoapp/new_app/news/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import NewListView, NewsDetailView
+from .views import NewsListView, NewsDetailView, NewsCreateView, NewsUpdateView
 
 urlpatterns = [
-    path('', NewListView.as_view(), name='news_list'),  # Список новостей
-    path('<int:pk>/', NewsDetailView.as_view(), name='news_detail'),  # Детальная новость
+    path('', NewsListView.as_view(), name='news_list'),  # Список новостей
+    path('<int:pk>/', NewsDetailView.as_view(), name='news_detail'),
+    path('add/', NewsCreateView.as_view(), name='news_add'),
+    path('<int:pk>/edit/', NewsUpdateView.as_view(), name='news_edit'),
+    # Детальная новость
+
+# http://127.0.0.1:8000/news/add
+    # http://127.0.0.1:8000/news/2/edit
 
 
     #http://127.0.0.1:8000/news/2/
--- djangoapp/new_app/news/views.py
+++ djangoapp/new_app/news/views.py
@@ -3,12 +3,42 @@ from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from django.views import View
 from .models import News, Comment
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .models import News
+from django.views.generic import ListView, DetailView, UpdateView
+from .forms import NewsForm
 
 
-class NewListView(View):
-    def get(self, request):
-        news = News.objects.all().order_by('-created_at')
-        return render(request, 'news/news_list.html', {'news': news})
+
+# views.py
+from django.shortcuts import redirect
+
+def home(request):
+    return redirect('news_list')
+
+class NewsCreateView(CreateView):
+    model = News
+    fields = ['title', 'content']
+    template_name = 'news/news_form.html'
+    success_url = reverse_lazy('news_list')
+
+# class NewsCreateView(View):
+#     def get(self, request):
+#         form = NewsForm()
+#         return render(request, 'news/news_form.html', {'form': form})
+#     def post(self,request):
+#         form = NewsForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('news_list')
+#         return render(request, 'news/news_form.html', {'form': form})
+
+
+class NewsListView(ListView):
+    model = News
+    template_name = 'news/news_list.html'  # Создайте этот шаблон для отображения списка новостей
+    context_object_name = 'news_list'
 
 class NewsDetailView(View):
     def get(self,request,pk):
@@ -24,4 +54,24 @@ class NewsDetailView(View):
 
         return redirect('news_detail', pk=pk)
 
+class NewsUpdateView(View):
+    def get(self, request, pk):
+        news_item = get_object_or_404(News, pk=pk)
+        form = NewsForm(instance=news_item)
+        return render(request, 'news/news_form.html', {'form': form})
 
+    def post(self, request, pk):
+        news_item = get_object_or_404(News, pk=pk)
+        form = NewsForm(request.POST, instance=news_item)
+        if form.is_valid():
+            form.save()  # сохраняем обновления новости
+            return redirect('news_detail', pk=news_item.pk)  # перенаправляем на страницу новости
+        return render(request, 'news/news_form.html', {'form': form})
+
+# class NewsDetailView(DetailView):
+#     model = News
+#     template_name = 'news/news_detail.html'
+#     context_object_name = 'news'
+
+    def get_success_url(self):
+        return redirect('news_detail', pk=self.object.pk)
