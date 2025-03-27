1fda43e46359446ec20b33d7294ac27a9f87f6d3
Nurs <nursultanerbakytuly88@gmail.com>
2025-03-27 14:24:19 +0500

    Project4

--- djangoapp/new_app/news/views.py
+++ djangoapp/new_app/news/views.py
@@ -1,15 +1,17 @@
 
-from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.views import View
 from .models import News, Comment
-from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
 from .models import News
 from django.views.generic import ListView, DetailView, UpdateView
-from .forms import NewsForm
-
-
+from .forms import NewsForm, CommentForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 # views.py
 from django.shortcuts import redirect
@@ -17,11 +19,13 @@ from django.shortcuts import redirect
 def home(request):
     return redirect('news_list')
 
-class NewsCreateView(CreateView):
+class NewsCreateView(LoginRequiredMixin, CreateView):
     model = News
     fields = ['title', 'content']
     template_name = 'news/news_form.html'
     success_url = reverse_lazy('news_list')
+
+
 
 # class NewsCreateView(View):
 #     def get(self, request):
@@ -68,10 +72,69 @@ class NewsUpdateView(View):
             return redirect('news_detail', pk=news_item.pk)  # перенаправляем на страницу новости
         return render(request, 'news/news_form.html', {'form': form})
 
-# class NewsDetailView(DetailView):
-#     model = News
-#     template_name = 'news/news_detail.html'
-#     context_object_name = 'news'
+    # class NewsDetailView(DetailView):
+    #     model = News
+    #     template_name = 'news/news_detail.html'
+    #     context_object_name = 'news'
 
     def get_success_url(self):
         return redirect('news_detail', pk=self.object.pk)
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/sign_up.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        user = form.save()  # Сначала сохраняем пользователя
+        default_group, created = Group.objects.get_or_create(name='Обычные пользователи')
+        user.groups.add(default_group)  # Добавляем пользователя в группу
+        return redirect(self.success_url)
+
+
+@login_required
+def news_add(request):
+    if request.method == "POST":
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            news = form.save(commit=False)
+            news.author = request.user  # Привязка к текущему пользователю
+            news.save()
+            return redirect('news_list')
+    else:
+        form = NewsForm()
+    return render(request, 'news/news_form.html', {'form': form})
+
+@login_required
+def comment_add(request, news_id):
+    news = get_object_or_404(News, id=news_id)  # Проверяем существование новости
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.news = news
+            comment.user = request.user  # Привязываем к текущему пользователю
+            comment.save()
+            return redirect('news_detail', pk=news_id)
+    else:
+        form = CommentForm()
+    return render(request, 'news/comment_form.html', {'form': form, 'news': news})
+
+
+
+
+
+class CommentAddView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'news/comment_form.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.news = get_object_or_404(News, id=self.kwargs['pk'])  # Исправляем ID
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('news_detail', kwargs={'pk': self.kwargs['pk']})
+
